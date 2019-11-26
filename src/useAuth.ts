cdde1192c41dd9f064144ecc08af9ba14f4e75dc
sorenhoyer <sorenhoyer84@gmail.com>
2019-11-26 22:11:25 +0100

    Fix import path for Provider after removing prefix in last commit

--- src/useAuth.ts
+++ src/useAuth.ts
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { AuthContext } from './AuthProvider';
+import { AuthContext } from './Provider';
 import { ContextProps } from './types';
 
 const useAuth = (): ContextProps => {
